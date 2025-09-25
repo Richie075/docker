8dd83ff1edbabaca057db92d2ec771b4bd064129
Richie075 <46599476+Richie075@users.noreply.github.com>
2025-09-25 14:21:42 +0200

    Update create.sql

--- clickhouse_api/create.sql
+++ clickhouse_api/create.sql
@@ -28,6 +28,23 @@ ENGINE = MergeTree()
 PARTITION BY toYYYYMM(timestamp)
 ORDER BY timestamp
 
+    CREATE VIEW processdata.moneothingwithrawdata
+ AS
+SELECT
+  thingid, 
+  uniqueidentifier,
+  displayname,
+  value, 
+  timestamp
+FROM (
+  SELECT
+    m.thingid as thingid, m.uniqueidentifier as uniqueidentifier, m.displayname as displayname, r.value as value, mr.timestamp as timestamp
+  FROM processdata.moneothingrawdata mr 
+  INNER JOIN processdata.moneothing m ON m.id = mr.thingid
+  INNER JOIN processdata.rawdata r ON r.id = mr.rawdataid
+)
+GROUP BY thingid, uniqueidentifier, displayname, timestamp, value
+    
 CREATE MATERIALIZED VIEW processdata.moneothingwithrawdata_mv
 TO processdata.moneothingwithrawdata AS
 SELECT
